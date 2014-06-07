09193cb33a6b922046dbaf6bb568b0dfbc27882e
dottavio <dottavio@gmail.com>
2014-06-07 19:24:28 -0300

    Updating loop

--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.render('profile', { title: 'Express'});
+	res.render('profile', { title: 'Express', products: [
+        {url:'http://www.olapic.com/1', image:'images.olapic.com/1'}, 
+        {url:'http://www.olapic.com/1', image:'images.olapic.com/1'}, 
+        {url:'http://www.olapic.com/1', image:'images.olapic.com/1'}, 
+        {url:'http://www.olapic.com/1', image:'images.olapic.com/1'}
+    ]});
 });
 
 module.exports = router;
