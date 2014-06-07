953e5a5a5738b23adc8f10adc78f3276afe2fbbd
Ramiro Castro <castro.ramiro@gmail.com>
2014-06-07 19:32:57 -0300

    profile

--- routes/profile.js
+++ routes/profile.js
@@ -4,10 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
 	res.render('profile', { title: 'Express', products: [
-        {url:'http://www.olapic.com/1', image:'images.olapic.com/1'}, 
-        {url:'http://www.olapic.com/1', image:'images.olapic.com/1'}, 
-        {url:'http://www.olapic.com/1', image:'images.olapic.com/1'}, 
-        {url:'http://www.olapic.com/1', image:'images.olapic.com/1'}
+        {name: "Product 1", url:'http://www.olapic.com/1', image:'img/portfolio/pix1-th.jpg'}, 
+        {name: "Product 2", url:'http://www.olapic.com/2', image:'img/portfolio/pix1-th.jpg'}, 
+        {name: "Product 3", url:'http://www.olapic.com/3', image:'img/portfolio/pix1-th.jpg'}, 
+        {name: "Product 4", url:'http://www.olapic.com/4', image:'img/portfolio/pix1-th.jpg'}
     ]});
 });
 
